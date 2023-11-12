8df7c2ae587909dd3d84eafbb8fba52f84a631e0
Chakradhar Cholleti <cholletichakradhar@gmail.com>
2023-11-13 01:19:24 +0530

    Update firebase.jsx

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -8,14 +8,14 @@ import {getDatabase, ref, onValue} from 'firebase/database';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCYTCA2VenslHO6Gob_C7o1a4TbPmIlhsE',
-  authDomain: 'user-friendly-ai-assist.firebaseapp.com',
-  databaseURL: 'https://user-friendly-ai-assist-default-rtdb.firebaseio.com',
-  projectId: 'user-friendly-ai-assist',
-  storageBucket: 'user-friendly-ai-assist.appspot.com',
-  messagingSenderId: '56918199077',
-  appId: '1:56918199077:web:952fa51b3cbfa0e3dbe8cc',
-  measurementId: 'G-GC4E29KXT5',
+  apiKey: 'api-key-here',
+  authDomain: 'firebaseAuth',
+  databaseURL: 'firebaseDatabase',
+  projectId: 'firebaseProject',
+  storageBucket: 'firebaseBucket',
+  messagingSenderId: 'firebaseMsgSender',
+  appId: 'firebaseAppId',
+  measurementId: 'firebaseMeasurementId',
 };
 
 // Initialize Firebase
